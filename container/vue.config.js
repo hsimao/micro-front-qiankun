f88f20391067bef10d524144e83094a006ee2a0f
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-06-12 22:45:21 +0800

    feat: container publicPath update

--- container/vue.config.js
+++ container/vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   devServer: {
     port: 3000
   },
-  publicPath: "./"
+  // 部署位置 github page 根目錄 /micro-front-qiankun/
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/micro-front-qiankun/" : "/"
 };
